983d39563beab8101113cc639b809b96fd828fe5
WanderingLemon <wisdomcr9@gmail.com>
2023-10-19 23:47:56 -0400

    Added docstring for APICourse

--- src/API/Courses/APICourse.ts
+++ src/API/Courses/APICourse.ts
@@ -1,6 +1,18 @@
 import { APIUser } from "../Users/APIUser";
 import { APIMeeting } from "../Meetings/APIMeeting";
 
+/**
+ * Represents an RIT course object
+ * @interface Course 
+ * @param section - The section of the course
+ * @param name - The name of the course
+ * @param term - The term that the course is being taught
+ * @param instructors - The instructor(s) of the course as APIUser data.
+ * @param students - Number of students enrolled in course
+ * @param meetings - Meetings of the course as APIMeeting data
+ * @see APIUser
+ * @see APIMeeting
+ */
 export interface APICourse {
     section: string,
     groupname: string,
