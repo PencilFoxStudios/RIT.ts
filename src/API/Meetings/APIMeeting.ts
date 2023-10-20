03f4e5ffecca17d71e4d5d1c76580628ab6a7f46
WanderingLemon <wisdomcr9@gmail.com>
2023-10-19 23:49:06 -0400

    Added docstring

--- src/API/Meetings/APIMeeting.ts
+++ src/API/Meetings/APIMeeting.ts
@@ -1,6 +1,24 @@
 import { APIRoom } from "../Rooms/APIRoom";
 import { MeetingType } from "./MeetingType";
 
+/**
+ * Represents a meeting of an RIT course
+ * @interface Meeting
+ * @param id - The ID of the meeting in the RIT system
+ * @param date - The date that the meeing will take place. Format: yyyy-mm-dd
+ * @param start - Start time of the meeting
+ * @param end - End time of the meeting
+ * @param meeting - The given name of the meeting
+ * @param meetingtype - The type of meeting represented by the MeetingType enum
+ * @param facilitator - The facilitator of the meeting.
+ * @param room_id - The ID of the room the meeting is to be held in
+ * @param created_at - The time that the meeting was created in the system
+ * @param updated_at - Time that the meeting was last updated in the system
+ * @param room - The room that the meeting is to be held in as APIRoom data
+ * 
+ * @see MeetingType
+ * @see APIRoom
+ */
 export interface APIMeeting {
     id: string,
     date: string,
