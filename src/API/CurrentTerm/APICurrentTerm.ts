c40360a04a3f4864ad338f8d8f4793e39f0200bf
WanderingLemon <wisdomcr9@gmail.com>
2023-10-19 23:48:28 -0400

    Added docstring

--- src/API/CurrentTerm/APICurrentTerm.ts
+++ src/API/CurrentTerm/APICurrentTerm.ts
@@ -1,3 +1,10 @@
+/**
+ * Represents data associated with a term at RIT
+ * @param currentTerm - The ID of the current RIT term
+ * @param description - The description of the term
+ * @param start_date - The date that the term starts
+ * @param end_date - The date that the term ends
+ */
 export interface APICurrentTerm {
     currentTerm: string,
     description: string,
